0a87819ae3d8d0ea6047a0738a6df1399580191c
bugger69 <aatreya21@gmail.com>
2022-06-15 20:49:56 +0530

    final tweaks, now css time

--- app.js
+++ app.js
@@ -14,10 +14,14 @@ function btnRole(btn) {
     } else if (btn.classList.contains('done')) {
         btn.addEventListener('click', () => {
             if(btn.innerText === "Done") {
-                btn.parentElement.classList.add('linethr');
+                btn.parentElement.classList.add("finished");
+                let task = btn.parentElement.querySelector('.task');
+                task.classList.add('linethr');
                 btn.innerText = "Undo";
             } else if (btn.innerText === "Undo"){
-                btn.parentElement.classList.remove('linethr');
+                btn.parentElement.classList.remove("finished");
+                let task = btn.parentElement.querySelector('.task');
+                task.classList.remove('linethr');
                 btn.innerText = "Done";
             }
         })
@@ -28,12 +32,15 @@ submit.addEventListener('click', () => {
     if(input.value !== "") {
         const li = document.createElement('li');
         const  del = document.createElement('button');
+        const span = document.createElement('span');
+        span.innerText = input.value;
+        span.classList.add('task');
         del.innerText = "delete";
         del.classList.add('del');
         const done = document.createElement('button');
         done.innerText = "Done";
         done.classList.add('done');
-        li.append(input.value);
+        li.append(span);
         li.append(done);
         li.append(del);
         btnRole(del);
@@ -46,7 +53,8 @@ submit.addEventListener('click', () => {
 remdone.addEventListener('click', () => {
     const lis = document.querySelectorAll("li");
     for(let li of lis) {
-        if(li.classList.contains('linethr')) {
+        if(li.classList.contains('finished')) {
+            console.log(li.classList);
             li.remove();
         }
     }
